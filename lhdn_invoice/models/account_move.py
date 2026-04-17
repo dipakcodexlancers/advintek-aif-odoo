e72c84c1cdd2ed3fcbb4c7bd2b48f4ccb5773603
Dipak Borkhatariya <dipak.codexlancers@gmail.com>
2026-04-17 12:24:58 +0530

    Add submission detail page

--- lhdn_invoice/models/account_move.py
+++ lhdn_invoice/models/account_move.py
@@ -1,8 +1,16 @@
-from odoo import models
+from odoo import models, fields
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    # Button action
     def action_submit_irbm(self):
         for rec in self:
             rec.message_post(body="IRBM Button Clicked")
+
+    # Fields
+    lhdn_status = fields.Char(string="Status")
+    lhdn_uuid = fields.Char(string="UUID")
+    lhdn_validation_date = fields.Datetime(string="Validation Date")
+    lhdn_validation_link = fields.Char(string="Validation Link")
+    lhdn_rejection_result = fields.Text(string="Rejection Result")
