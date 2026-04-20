fc97fbaad121d31326a7e5a2ff91215ce0f18232
Dipak Borkhatariya <dipak.codexlancers@gmail.com>
2026-04-20 09:02:50 +0530

    add InvoiceTypeCode

--- lhdn_invoice/models/account_move.py
+++ lhdn_invoice/models/account_move.py
@@ -10,45 +10,8 @@ class AccountMove(models.Model):
     def action_submit_irbm(self):
         for rec in self:
 
-            # payload = {
-            #     "invoice": {
-            #         "number": rec.name,
-            #         "date": str(rec.invoice_date) if rec.invoice_date else "",
-            #         "total": rec.amount_total,
-            #         "currency": rec.currency_id.name,
-            #     },
-            #     "customer": {
-            #         "name": rec.partner_id.name,
-            #         "email": rec.partner_id.email,
-            #         "phone": rec.partner_id.phone,
-            #         "tin": rec.partner_id.lhdn_tin,
-            #         "id_type": rec.partner_id.lhdn_id_type,
-            #         "id_value": rec.partner_id.lhdn_id_value,
-            #     },
-            #     "company": {
-            #         "name": rec.company_id.name,
-            #         "email": rec.company_id.email,
-            #         "phone": rec.company_id.phone,
-            #         "tin": rec.company_id.lhdn_tin,
-            #         "id_type": rec.company_id.lhdn_id_type,
-            #         "id_value": rec.company_id.lhdn_id_value,
-            #         "msic_code": rec.company_id.lhdn_msic_code,
-            #         "business_activity": rec.company_id.lhdn_business_activity,
-            #     },
-            #     "lines": [
-            #         {
-            #             "description": line.name,
-            #             "quantity": line.quantity,
-            #             "price": line.price_unit,
-            #             "subtotal": line.price_subtotal,
-            #             "tax": [t.name for t in line.tax_ids],
-            #         }
-            #         for line in rec.invoice_line_ids
-            #     ]
-            # }
-
             invoice_data = rec.read()[0]
-            
+
             payload = {
                 # "invoice": rec.read()[0],
                 "invoice": {
@@ -75,6 +38,15 @@ class AccountMove(models.Model):
                 # ]
             }
 
+            invoice_type_map = {
+                "out_invoice": "01",
+                "out_refund": "02",
+                "in_invoice": "11",
+                "in_refund": "12",
+            }
+
+            invoice_type_code = invoice_type_map.get(rec.move_type)
+
             url = "https://espresso-freezable-dealing.ngrok-free.dev/Login/Submit"
             
             headers = {
@@ -82,36 +54,26 @@ class AccountMove(models.Model):
             }
 
             try:
-                # rec.message_post(body=f"IRBM Payload: {json.dumps(payload)}")
+
+                rec.message_post(body=f"IRBM Payload: {json.dumps(payload, default=str)}")
 
                 # response = requests.post(
                 #     url,
                 #     headers=headers,
-                #     data=json.dumps(payload),
+                #     data=json.dumps(payload, default=str),
                 #     timeout=10000
                 # )
-                rec.message_post(body=f"IRBM Payload: {json.dumps(payload, default=str)}")
 
                 response = requests.post(
                     url,
                     headers=headers,
+                    params={"InvoiceTypeCode": invoice_type_code},
                     data=json.dumps(payload, default=str),
                     timeout=10000
                 )
 
                 rec.message_post(body=f"IRBM Response Status: {response.status_code}")
                 rec.message_post(body=f"IRBM Response Body: {response.text}")
-
-                # if response.status_code == 200:
-                #     data = response.json()
-
-                #     rec.lhdn_status = "Submitted"
-                #     rec.lhdn_uuid = data.get("uuid")
-                #     rec.lhdn_validation_link = data.get("validation_link")
-                #     rec.lhdn_validation_date = datetime.now()
-                #     rec.lhdn_rejection_result = False
-
-                #     rec.message_post(body="IRBM Submitted Successfully")
 
                 if response.status_code == 200:
                     data = response.json()
@@ -124,17 +86,10 @@ class AccountMove(models.Model):
 
                     rec.lhdn_rejection_result = data.get("rejection_result")
 
-                    # UI message
                     if data.get("status") == "Success":
                         rec.message_post(body="IRBM Submitted Successfully")
                     else:
                         rec.message_post(body=f"IRBM Failed: {data.get('message')}")
-
-                # else:
-                #     rec.lhdn_status = "Failed"
-                #     rec.lhdn_rejection_result = response.text
-
-                #     rec.message_post(body="IRBM Submission Failed")
 
                 else:
                     try:
@@ -148,11 +103,6 @@ class AccountMove(models.Model):
 
                     rec.message_post(body=f"IRBM Submission Failed: {error_msg}")
 
-            # except Exception as e:
-            #     rec.lhdn_status = "Error"
-            #     rec.lhdn_rejection_result = str(e)
-
-            #     rec.message_post(body=f"IRBM Error: {str(e)}")
             except Exception as e:
                 rec.lhdn_status = "Error"
                 rec.lhdn_rejection_result = str(e)
